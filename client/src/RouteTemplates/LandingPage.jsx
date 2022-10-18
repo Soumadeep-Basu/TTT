f087ba10987892a939f36944b6b1777d2cfb9833
soumadeepbasu <soumadeepbasu98@gmail.com>
2022-10-17 19:05:55 -0500

    Added Landing Page

---
+++ client/src/RouteTemplates/LandingPage.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import Content from '../ContentTemplates/LandingPageContent';
+import './LandingPage.css';
+
+
+
+
+
+
+function Test() {
+
+    return (
+
+
+        <Content />
+
+
+
+    );
+}
+
+
+
+
+
+export default Test;
